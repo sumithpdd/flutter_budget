bed8936b40c1ab2aac5ca232d188281d39aa49b1
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-05-09 00:03:34 +0200

    Paint Expense radial progress

---
+++ lib/screens/category_screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_budget_ui/helpers/color_helper.dart';
+import 'package:flutter_budget_ui/widgets/radial_painter.dart';
+
+import '../models/category_model.dart';
+import '../models/expense_model.dart';
+
+class CategoryScreen extends StatefulWidget {
+  final Category category;
+  CategoryScreen({this.category});
+
+  @override
+  _CategoryScreenState createState() => _CategoryScreenState();
+}
+
+class _CategoryScreenState extends State<CategoryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    double totalAmountSpent = 0;
+    widget.category.expenses.forEach((Expense expense) {
+      totalAmountSpent += expense.cost;
+    });
+    final double amountLeft = widget.category.maxAmount - totalAmountSpent;
+    final double percent = amountLeft / widget.category.maxAmount;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.category.name),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            iconSize: 30.0,
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(20.0),
+              padding: EdgeInsets.all(20.0),
+              height: 250.0,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(18.0),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 6.0,
+                      color: Colors.black12,
+                      offset: Offset(0, 2),
+                    )
+                  ]),
+              child: CustomPaint(
+                foregroundPainter: RadialPainter(
+                  bgColor: Colors.grey[200],
+                  lineColor: getColor(context, percent),
+                  percent: percent,
+                  width: 15.0,
+                ),
+                child: Center(
+                  child: Text(
+                      '\$${amountLeft.toStringAsFixed(2)} / \$${widget.category.maxAmount}'),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
