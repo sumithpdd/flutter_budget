c571b4de37e3da08723afd509dbc201d44658e46
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-05-08 18:54:32 +0200

    Commit Barchart working

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_budget_ui/widgets/bar_chart.dart';
+
+import '../data/data.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            forceElevated: true,
+            floating: true,
+            // pinned: true,
+            expandedHeight: 100.0,
+            leading: IconButton(
+              icon: Icon(Icons.settings),
+              iconSize: 30.0,
+              onPressed: () {},
+            ),
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text('Simple Budget'),
+            ),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.add),
+                iconSize: 30.0,
+                onPressed: () {},
+              )
+            ],
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  margin: EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12,
+                        offset: Offset(0, 2),
+                        blurRadius: 6.0,
+                      ),
+                    ],
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: BarChart(expenses:weeklySpending),
+                );
+              },
+              childCount: 1,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/bar_chart.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+class BarChart extends StatelessWidget {
+  final List<double> expenses;
+  BarChart({this.expenses});
+
+  @override
+  Widget build(BuildContext context) {
+    double mostExpensive = 0;
+    expenses.forEach((double price) {
+      if (price > mostExpensive) {
+        mostExpensive = price;
+      }
+    });
+    List<String> week = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Column(
+        children: <Widget>[
+          Text(
+            'Weekly Spending',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1.2,
+            ),
+          ),
+          SizedBox(height: 5.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.arrow_back),
+                iconSize: 30.0,
+                onPressed: () {},
+              ),
+              Text(
+                'May 3, 2020 - May 9, 2020',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 1.2,
+                ),
+              ),
+              IconButton(
+                icon: Icon(Icons.arrow_forward),
+                iconSize: 30.0,
+                onPressed: () {},
+              )
+            ],
+          ),
+          SizedBox(height: 30.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              for (var i = 0; i < week.length; i++)
+                Bar(
+                  label: week[i],
+                  amountSpent: expenses[i],
+                  mostExpensive: mostExpensive,
+                )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Bar extends StatelessWidget {
+  final String label;
+  final double amountSpent;
+  final double mostExpensive;
+  final double _maxBarHeight = 150.0;
+
+  const Bar({this.label, this.amountSpent, this.mostExpensive});
+
+  @override
+  Widget build(BuildContext context) {
+    final barHeight = amountSpent / mostExpensive * _maxBarHeight;
+    return Column(
+      children: <Widget>[
+        Text(
+          '\$${amountSpent.toStringAsFixed(2)}',
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        SizedBox(height: 6.0),
+        Container(
+          height: barHeight,
+          width: 18.0,
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+            borderRadius: BorderRadius.circular(3.0),
+          ),
+        ),
+        SizedBox(height: 8.0),
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ],
+    );
+  }
+}
